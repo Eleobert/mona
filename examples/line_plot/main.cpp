0c58d678a7f76de3d3ad41bdadbe3702780a6d46
eleobert <eleobert@hotmail.com>
2021-11-17 19:56:04 +0300

    fix adding lines at each iteration

--- examples/line_plot/main.cpp
+++ examples/line_plot/main.cpp
@@ -21,16 +21,19 @@ int main()
     auto [w, h] = target.size();
     auto cam = mona::camera(w, h); // viewport should be set on control camera
     auto axes = mona::axes();
+
     auto line1 = mona::line(x, y, mona::colors::midnight_blue);
     auto line2 = mona::line(x, y % x, mona::colors::orange_red);
-    auto line3 = mona::line(x, y - x, mona::colors::black);
+    auto line3 = mona::line(x, y - x, mona::colors::pale_violet_red);
 
+    axes.add(line1);
+    axes.add(line2);
+    axes.add(line3);
+
+    float f = 0.0;
     while (target.active())
     {
         cam = target.control_camera(cam);
-        axes.add(line1);
-        axes.add(line2);
-        axes.add(line3);
         axes.draw(cam, target); // we sould not need camera here
         target.draw();
     }
