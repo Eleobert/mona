41c45dad4a609330e329dfefc945a9f0be8552f7
eleobert <eleobert@hotmail.com>
2021-11-25 16:28:44 +0300

    upgrade grid_view example

--- examples/grid_view/main.cpp
+++ examples/grid_view/main.cpp
@@ -1,11 +1,15 @@
-#include <armadillo>
-
 #include <mona/utility.hpp>
 #include <mona/line.hpp>
 #include <mona/targets/window.hpp>
 #include <mona/axes.hpp>
+#include <mona/axes3.hpp>
 #include <mona/colors.hpp>
 #include <mona/grid.hpp>
+#include <mona/surface_mesh.hpp>
+#include <mona/camera_rotation_control.hpp>
+
+#include <armadillo>
+
 
 auto fit(const arma::fvec& x, const arma::fvec& y) -> arma::fvec
 {
@@ -34,7 +38,7 @@ auto random(arma::fvec x, float mean, float std)
 
 auto get_dots()
 {
-    arma::fvec x = mona::linspace(-4, 4, 15);
+    arma::fvec x = mona::linspace(-4, 4, 30);
     arma::fvec y = random(x, 0, 0.9);
     return mona::dots(x, y, mona::colors::tomato);
 }
@@ -53,6 +57,29 @@ auto get_line()
     return mona::line(x, y, mona::colors::midnight_blue);
 }
 
+
+auto f1(double x, double y)
+{
+    return (7 * x * y) / std::exp(x*x + y*y);
+};
+
+auto f2(double x, double y)
+{
+    return std::pow(x, 4) + std::pow(y, 4) - std::pow(x + y, 2);
+};
+
+template <typename F>
+auto get_surface(F f)
+{
+    arma::fvec line = mona::linspace(-2, 2, 50);
+    auto [x, y] = mona::meshgrid(line, line);
+    auto z = mona::apply(f, x, y);
+
+    auto mesh = mona::surface_mesh(x, y, z);
+
+    return mesh;
+}
+
 int main()
 {
     auto win  = mona::targets::window();
@@ -60,18 +87,27 @@ int main()
     auto cam  = mona::camera();
     auto axes_dots = mona::axes();
     auto axes_line = mona::axes();
+    auto axes_surf1 = mona::axes3({-4, 4}, {-4, 4}, {-4, 4}, 5);
+    axes_surf1.set_camera_control(win.get_camera_control());
+
+    auto axes_surf2 = mona::axes3({-4, 4}, {-4, 4}, {-4, 4}, 5);
+    axes_surf2.set_camera_control(mona::camera_rotation_control(0.2f, 0.f));
 
     auto dots = get_dots();
     auto line = get_line();
+    auto surf1 = get_surface(f1);
+    auto surf2 = get_surface(f2);
 
     while (win.active())
     {
         axes_dots.submit(dots);
         axes_line.submit(line);
+        axes_surf1.submit(surf1);
+        axes_surf2.submit(surf2);
 
         view(0, 0).submit(axes_dots);
-        view(0, 1).submit(axes_line);
-        view(1, 0).submit(axes_dots);
+        view(0, 1).submit(axes_surf1);
+        view(1, 0).submit(axes_surf2);
         view(1, 1).submit(axes_line);
 
         win.submit(view);
