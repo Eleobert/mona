91e54f9dc891367f5011adf925cd7c13cc217839
eleobert <eleobert@hotmail.com>
2021-11-25 14:30:43 +0300

    remove surface.cpp file

--- examples/surface_plot/main.cpp
+++ examples/surface_plot/main.cpp
@@ -2,6 +2,9 @@
 
 #include <mona/utility.hpp>
 #include <mona/plot/surface.hpp>
+#include <mona/targets/window.hpp>
+#include <mona/surface_mesh.hpp>
+#include <mona/axes3.hpp>
 
 auto f(double x, double y)
 {
@@ -12,8 +15,17 @@ int main()
 {
     arma::fvec line = mona::linspace(-2, 2, 50);
     auto [x, y] = mona::meshgrid(line, line);
-
     auto z = mona::apply(f, x, y);
 
-    mona::plot::surface(x, y, z);
+    auto win = mona::targets::window();
+    auto mesh = mona::surface_mesh(x, y, z);
+    auto axes = mona::axes3({x.min(), x.max()}, {y.min(), y.max()}, {z.min(), z.max()}, 5);
+    axes.set_camera_control(win.get_camera_control());
+
+    while (win.active())
+    {
+        axes.submit(mesh);
+        win.submit(axes);
+        win.draw();
+    }
 }
